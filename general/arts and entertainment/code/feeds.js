7b1864567edc55b7f4f1f1d4e021848e814bf175
Jamie Dawson <jadawson@e1z3r7p4.42.us.org>
2019-10-12 15:47:07 -0700

    art and entertainment

---
+++ general/arts and entertainment/code/feeds.js
@@ -0,0 +1,18 @@
+// Add RSS feeds below
+// Url: Link to RSS feed xml (url must start with 'https://') (Required)
+// urlText: Text  for hyperlink shown at bottom of article (Required)
+// Text: What users see in feed selection list (Required)
+// Description: A description of what this is link contains.(Optional)
+// Tags: What the user can say to automatically call certain feeds (Required)
+// image: image shown next to items in category list (can be a local path or directory) (Optional)
+// Copyright: Feeds copyright message to use if the feed doesn't have one (Optional)
+module.exports = module.exports = [
+  {
+    url: 'https://rss.cbc.ca/lineup/arts.xml',
+    urlText: 'Click here to see the full story on cbc.ca',
+    text: 'CBC Art and Entertainment',
+    image: './icon.png',
+    description: "CBC news stories About Art and Entertainment.",
+    tags: ['canada', 'cbc','top stories', 'art', 'entertainment', 'fun', 'movies', 'video games']
+  }
+]
