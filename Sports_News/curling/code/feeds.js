cf722ca7488925b80fe9b3bd693742f7f6297ff5
Jamie Dawson <jadawson@e1z3r7p4.42.us.org>
2019-10-12 16:30:05 -0700

    curling

---
+++ Sports_News/curling/code/feeds.js
@@ -0,0 +1,18 @@
+// Add RSS feeds below
+// Url: Link to RSS feed xml (url must start with 'https://') (Required)
+// urlText: Text  for hyperlink shown at bottom of article (Required)
+// Text: What users see in feed selection list (Required)
+// Description: A description of what this is link contains.(Optional)
+// Tags: What the user can say to automatically call certain feeds (Required)
+// image: image shown next to items in category list (can be a local path or directory) (Optional)
+// Copyright: Feeds copyright message to use if the feed doesn't have one (Optional)
+module.exports = module.exports = [
+  {
+    url: 'https://rss.cbc.ca/lineup/sports-curling.xml',
+    urlText: 'Click here to see the full story on cbc.ca',
+    text: 'CBC NBA',
+    image: './icon.png',
+    description: "The latest news from CBC about curling.",
+    tags: ['canada', 'cbc','top stories', 'sports', 'curling']
+  }
+]
